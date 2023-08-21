45a3e94452a608ee7fe7acd2cda1e61f604d8e8b
Chintan Aparnathi <chintanaparnathi48@gmail.com>
2023-08-21 23:12:49 +0530

    pr5

--- app/src/main/java/com/example/mad_practical5_21012011002/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical5_21012011002/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mad_practical5_21012011002
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -12,64 +11,77 @@ import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val Browse : Button=findViewById(R.id.Button1)
-        Browse.setOnClickListener {
-            val editBrowser : EditText=findViewById(R.id.editTextBrowse)
 
-            Browser(editBrowser.text.toString())
-        }
-        val btnCall:Button = findViewById(R.id.btnCall)
-        val editCall:EditText=findViewById(R.id.editTextPhone)
-        btnCall.setOnClickListener(){
-            call(editCall.text.toString())
-        }
-        val btnCallLog:Button=findViewById(R.id.btnCallLog)
-        btnCallLog.setOnClickListener(){
-            call(editCall.text.toString())
-        }
-        val gallery:Button=findViewById(R.id.btnGallery)
-        gallery.setOnClickListener(){
-            Gallery()
-        }
-        val Camera:Button=findViewById(R.id.btnCamera)
-        Camera.setOnClickListener(){
-            Camera()
-        }
-        val Alarm:Button=findViewById(R.id.btnalarm)
-        Alarm.setOnClickListener(){
-            Alarm()
+        val buttonBrowse:Button = findViewById(R.id.button_browse)
+        val editUrl:EditText = findViewById(R.id.editTextText_url)
+
+        val buttonPhone:Button = findViewById(R.id.button2_phone)
+        val editPhone:EditText = findViewById(R.id.editTextText7_phNo)
+
+        val buttonCallLog:Button = findViewById(R.id.button_callLog)
+
+        val buttonGallery:Button = findViewById(R.id.button4_gallery)
+
+        val buttonCamera:Button = findViewById(R.id.button5_camera)
+
+        val buttonAlarm:Button = findViewById(R.id.button6_alarm)
+
+        buttonBrowse.setOnClickListener {
+            openBrowser(editUrl.text.toString())
         }
 
+        buttonPhone.setOnClickListener {
+            makeCall(editPhone.text.toString())
+        }
+
+        buttonCallLog.setOnClickListener {
+            callLog()
+        }
+
+        buttonGallery.setOnClickListener {
+            gallery()
+        }
+
+        buttonCamera.setOnClickListener {
+            camera()
+        }
+
+        buttonAlarm.setOnClickListener {
+            alarm()
+        }
 
     }
 
-    private fun call(toString: String) {
+    fun openBrowser(link : String){
 
+        Intent(Intent.ACTION_VIEW , Uri.parse(link)).also { startActivity(it) }
     }
 
-    fun Browser(url : String){
-        Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { startActivity(it) }
+    fun makeCall(phNo : String){
+
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$phNo")).also { startActivity(it) }
     }
 
-    fun Call(number: Number){
-        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$number")).also { startActivity(it) }
+    fun callLog(){
 
-    }
-    fun call_log(){
         Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
-    fun Gallery(){
-        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
-    }
-    fun Camera(){
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
-    }
-    fun Alarm(){
-        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
+
+    fun gallery(){
+
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity((it)) }
     }
 
+    fun camera(){
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
+    }
+
+    fun alarm(){
+
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
+    }
 }
