7067e90426ca9cc7ec1352fa37c38c8e9a505bf9
Chintan Aparnathi <chintanaparnathi48@gmail.com>
2023-09-17 23:21:34 +0530

    pr5

--- app/src/main/java/com/example/mad_practical5_21012011002/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical5_21012011002/MainActivity.kt
@@ -5,83 +5,62 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
+import android.provider.Browser
 import android.provider.CallLog
+import android.provider.ContactsContract.DisplayPhoto
 import android.provider.MediaStore
+import android.telecom.Call
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Gallery
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonBrowse:Button = findViewById(R.id.button_browse)
-        val editUrl:EditText = findViewById(R.id.editTextText_url)
-
-        val buttonPhone:Button = findViewById(R.id.button2_phone)
-        val editPhone:EditText = findViewById(R.id.editTextText7_phNo)
-
-        val buttonCallLog:Button = findViewById(R.id.button_callLog)
-
-        val buttonGallery:Button = findViewById(R.id.button4_gallery)
-
-        val buttonCamera:Button = findViewById(R.id.button5_camera)
-
-        val buttonAlarm:Button = findViewById(R.id.button6_alarm)
-
-        buttonBrowse.setOnClickListener {
-            openBrowser(editUrl.text.toString())
+        val btnBrowser:Button=findViewById(R.id.buttonBrowser)
+        val editBrowser:EditText=findViewById(R.id.editTextBrowser)
+        btnBrowser.setOnClickListener {
+            Browser(editBrowser.text.toString())
         }
-
-        buttonPhone.setOnClickListener {
-            makeCall(editPhone.text.toString())
+        val btnCall:Button = findViewById(R.id.buttonCall)
+        val editCall:EditText=findViewById(R.id.editTextCall)
+        btnCall.setOnClickListener(){
+            call(editCall.text.toString())
         }
-
-        buttonCallLog.setOnClickListener {
-            callLog()
+        val btnCallLog:Button=findViewById(R.id.buttonCallLog)
+        btnCallLog.setOnClickListener(){
+            call(editCall.text.toString())
         }
-
-        buttonGallery.setOnClickListener {
+        val btnGallery:Button=findViewById(R.id.buttonGallery)
+        btnGallery.setOnClickListener(){
             gallery()
         }
-
-        buttonCamera.setOnClickListener {
+        val btnCamera:Button=findViewById(R.id.button2Camera)
+        btnCamera.setOnClickListener(){
             camera()
         }
-
-        buttonAlarm.setOnClickListener {
+        val btnAlarm:Button=findViewById(R.id.button3Alarm)
+        btnAlarm.setOnClickListener(){
             alarm()
         }
-
     }
-
-    fun openBrowser(link : String){
-
-        Intent(Intent.ACTION_VIEW , Uri.parse(link)).also { startActivity(it) }
+    fun Browser(url:String) {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { startActivity(it) }
     }
-
-    fun makeCall(phNo : String){
-
-        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$phNo")).also { startActivity(it) }
+    fun call(number:String){
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$number")).also{startActivity(it)}
     }
-
-    fun callLog(){
-
+    fun calllog(){
         Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
-
     fun gallery(){
-
-        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity((it)) }
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
     }
-
     fun camera(){
-
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
     }
-
     fun alarm(){
-
         Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
 }
